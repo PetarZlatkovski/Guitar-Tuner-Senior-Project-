b9d7fb72e5c0797d0212d623249a64b40a1b6ae5
Petar Zlatkovksi <123646933+PetarZlatkovski@users.noreply.github.com>
2026-02-15 17:37:20 +0200

    Small additions

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,11 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val textView = TextView(this)
+
+        textView.layoutParams= LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        textView.text = "A"
+        textView.text = "GeeksforGeeks"
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32f)
+        // onClick the text a message will be displayed "HELLO GEEK"
+        textView.setOnClickListener() {
+            Toast.makeText(this@MainActivity, "HELLO GEEK",
+                Toast.LENGTH_LONG).show()
+        }
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -17,4 +41,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
 }
